2013017114f0384a04f4e8ac8418235471e7a10a
NivekNK <kevin.rojas.v@mail.pucv.cl>
2023-02-24 01:26:05 -0300

    Initial commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,27 @@
+#region DEFINES: Delete if config is not required or needed.
+$TheLinuxWay = ""
+$ChangeAppsFolder = ""
+#endregion
+
+#region VARIABLES: Utility variables, don't delete.
+$CurrentUserPath = [Environment]::GetFolderPath('UserProfile') 
+$ProfilePath = Split-Path $PROFILE -Parent
+#endregion
+
+#region WRAPPER: Wrapper function to import plugins.
+function Import-Plugin {
+    param ([string]$PluginName)
+    Begin {
+        $PluginsPath = Join-Path $ProfilePath "plugins"
+    }
+    Process {
+        . "$PluginsPath\$PluginName"
+    }
+}
+#endregion
+
+Import-Plugin "Set-Shortcut.ps1"
+
+if (Test-Path variable:\ChangeAppsFolder) {
+    $ENV:SCOOP = "$CurrentUserPath\Apps\Scoop"
+}
---
+++ plugins/Set-Shortcut.ps1
@@ -0,0 +1,27 @@
+function Set-Shortcut {
+    param (
+        [Parameter(Mandatory=$true, HelpMessage="File to be linked.")]
+        [string]$Source,
+
+        [Parameter(Mandatory=$false, HelpMessage="Parameters used by the linked file.")]
+        [string]$ArgumentList,
+
+        [Parameter(Mandatory=$true, HelpMessage="Path to the linked file.")]
+        [string]$Destination
+    )
+    Begin {
+        $WshShell = New-Object -comObject WScript.Shell
+    }
+    Process {
+        $Shortcut = $WshShell.CreateShortcut($Destination)
+        $Shortcut.TargetPath = $Source
+        $Shortcut.Arguments = $ArgumentList
+    }
+    End {
+        $Shortcut.Save()
+    }
+}
+
+if (-not (Get-Alias ln -ErrorAction SilentlyContinue) -and (Test-Path variable:\TheLinuxWay)) {
+    New-Alias -Name ln -Value Set-Shortcut
+}
