29b8f7543f75b39aad8bc747b2b93a082fa7740a
NivekNK <kevin.rojas.v@mail.pucv.cl>
2023-02-24 21:48:22 -0300

    Added readme

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,6 +4,7 @@ $ChangeAppsFolder = ""
 #endregion
 
 #region VARIABLES: Utility variables, don't delete.
+$ENV:PYTHONIOENCODING = "utf-8"
 $CurrentUserPath = [Environment]::GetFolderPath('UserProfile') 
 $ProfilePath = Split-Path $PROFILE -Parent
 #endregion
@@ -20,7 +21,15 @@ function Import-Plugin {
 }
 #endregion
 
+#region PLUGINS
 Import-Plugin "Set-Shortcut.ps1"
+
+Invoke-Expression "$(thefuck --alias)"
+Invoke-Expression (& {
+    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
+    (zoxide init --hook $hook powershell | Out-String)
+})
+#endregion
 
 if (Test-Path variable:\ChangeAppsFolder) {
     $ENV:SCOOP = "$CurrentUserPath\Apps\Scoop"
---
+++ README.md
@@ -0,0 +1,10 @@
+# PowerShell Config
+
+## Requirements:
+
+* Install Python (pip).
+```code
+    winget install Python.Python.3.11
+```
+* Install [thefuck](https://github.com/nvbn/thefuck#installation)
+* Install [fzf](https://github.com/junegunn/fzf#windows)
